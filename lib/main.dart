ea260bef5feb731cb126c4ad61efa5a77303ff1e
Sadia5805 <tasnim15-5805@diu.edu.bd>
2025-02-19 11:21:30 +0600

    task001

--- lib/main.dart
+++ lib/main.dart
@@ -10,46 +10,53 @@ class MyWidget extends StatelessWidget {
     return MaterialApp(
       title: "Basic UI-Flutter",
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Basic Flutter"),
-            backgroundColor: const Color.fromARGB(74, 3, 15, 178),
-            foregroundColor: const Color.fromARGB(255, 196, 208, 26),
-            centerTitle: true,
-            elevation: 20.5,
-            shadowColor: Colors.blue,
-          ),
-          body: Row(
-            children: [
-              Icon(
-                Icons.star,
-                size: 60,
-                color: const Color.fromARGB(255, 214, 228, 16),
-              ),
-               Icon(
-                Icons.star,
-                size: 60,
-                color: const Color.fromARGB(255, 214, 228, 16),
-              ),
+        appBar: AppBar(
+          title: Text("Basic Flutter"),
+          backgroundColor: const Color.fromARGB(256, 17, 3, 62),
+          foregroundColor: const Color.fromARGB(255, 196, 208, 26),
+          centerTitle: true,
+          elevation: 20.5,
+          shadowColor: Colors.green,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
                 Icon(
                   Icons.star,
                   size: 60,
-                  color: const Color.fromARGB(90, 180, 22, 146),
+                  color: const Color.fromARGB(131, 192, 175, 43),
                 ),
+                Image.asset(
+              'assets/shoes.png', // Path to your image
+              width: 80, // Adjust width as needed
+              height: 80, // Adjust height as needed
+              fit: BoxFit.cover, // Adjust fit as needed
+            ),
                 Icon(
                   Icons.star,
                   size: 60,
-                  color: const Color.fromARGB(255, 173, 20, 20),
+                  color: const Color.fromARGB(124, 173, 160, 20),
                 ),
-                Icon(
-                  Icons.star,
-                  size: 60,
-                  color: const Color.fromARGB(255, 190, 25, 25),
-                ),
-            ],
-          )),
+              ],
+            ),
+            const SizedBox(height: 20), // Space between elements
+            // Image.asset(
+            //   'assets/shoes.png', // Path to your image
+            //   width: 300, // Adjust width as needed
+            //   height: 200, // Adjust height as needed
+            //   fit: BoxFit.cover, // Adjust fit as needed
+            // ),
+          ],
+        ),
+      ),
     );
   }
 }
+                
+            
 
 
 
